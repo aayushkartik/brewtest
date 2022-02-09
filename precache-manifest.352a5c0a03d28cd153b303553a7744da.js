0af04bcc8b4b8dba0c1533e587686ea56cd4cc6e
Aayushh Kartik <aayush.kartik123@gmail.com>
2022-02-10 00:44:53 +0530

    Updates

---
+++ precache-manifest.352a5c0a03d28cd153b303553a7744da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2b0c2b267d99bf19c5647603e5b701",
+    "url": "/brewtest/index.html"
+  },
+  {
+    "revision": "c39a203342be16542f04",
+    "url": "/brewtest/static/css/main.5d19a749.chunk.css"
+  },
+  {
+    "revision": "bfca9633e233249b603c",
+    "url": "/brewtest/static/js/2.c233d815.chunk.js"
+  },
+  {
+    "revision": "d03d05430b9a42cc73e3",
+    "url": "/brewtest/static/js/3.06a4cbf0.chunk.js"
+  },
+  {
+    "revision": "c39a203342be16542f04",
+    "url": "/brewtest/static/js/main.62f5ec99.chunk.js"
+  },
+  {
+    "revision": "a4213f59b80452edcf15",
+    "url": "/brewtest/static/js/runtime~main.e1707a15.js"
+  },
+  {
+    "revision": "a5fdee1980105569e141bcc2081dd314",
+    "url": "/brewtest/static/media/wallpaper.a5fdee19.png"
+  }
+]);
